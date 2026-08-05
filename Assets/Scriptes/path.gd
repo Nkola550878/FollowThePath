0d5245d5a27edd7cc02fbfb1d61f3e4e339bf116
Nikola Kosanovic <102851438+Nkola550878@users.noreply.github.com>
2026-08-05 23:03:48 +0200

    AddMid
    Adds point in wrong position

--- Assets/Scriptes/path.gd
+++ Assets/Scriptes/path.gd
@@ -3,10 +3,14 @@ extends Node2D
 @onready var line_2d: Line2D = $Line2D
 var gridSize = 7;
 var size = Vector2(490, 490);
-var lastClick : Vector2i;
+var tileSize : Vector2;
+var lastClick : Vector2i = Vector2i(100, 0);
+
+@export var pathNode : PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	tileSize = size / gridSize;
 	DrawPath()
 	pass # Replace with function body.
 
@@ -22,18 +26,46 @@ func _process(delta: float) -> void:
 		currentClick = MouseGridPosition();
 		if ((currentClick - lastClick).x - (currentClick - lastClick).y == 0 
 		or (currentClick - lastClick).x + (currentClick - lastClick).y == 0):
-			print("cut");
+			var k = (lastClick.y - currentClick.y) / (lastClick.x - currentClick.x);
+			var n = (lastClick.x * currentClick.y - currentClick.x * lastClick.y) / (lastClick.x - currentClick.x);
+			Mirror(k, n);
 			#insert cutting code
 			pass
 		lastClick = currentClick;
 
+func Mirror(k, n) -> void:
+	var i : int = 1;
+	while (i < self.get_child_count() - 1):
+		var child1GridPos = WorldToGrid(self.get_child(i).global_position);
+		var child2GridPos = WorldToGrid(self.get_child(i + 1).global_position);
+		var intersection : Vector2;
+		if (child1GridPos == child2GridPos):
+			i += 1;
+			continue;
+		if (child1GridPos.x == child2GridPos.x):
+			intersection = Vector2(child1GridPos.x, k * child1GridPos.x + n);
+			var instance = pathNode.instantiate();
+			instance.position = intersection;
+			add_child(instance);
+			move_child(instance, i + 1);
+			print(intersection);
+		if (child1GridPos.y == child2GridPos.y):
+			print("y");
+		i += 1;
+
+func WorldToGrid(pos) -> Vector2:
+	pos -= position;
+	pos += size / 2;
+	pos = pos.round();
+	return pos / tileSize;
+
+func GridToWorld(pos) -> Vector2:
+	pos = pos * tileSize;
+	return Vector2(0, 0);
+	pass;
+
 func MouseGridPosition() -> Vector2i:
 	var mousePosition : Vector2 = get_viewport().get_mouse_position();
-	mousePosition -= position;
-	mousePosition += size / 2;
-	mousePosition = mousePosition.round();
-	var gridPosition : Vector2i;
-	var tileSize = size / gridSize;
-	gridPosition = (mousePosition / tileSize).round();
+	var gridPosition = WorldToGrid(mousePosition).round()
 	return gridPosition
 	pass;
